b40e40048669dc85ca204ab30b635291f888b534
wyattscarpenter <wyattscarpenter@gmail.com>
2024-08-13 21:07:09 -0700

    actually, do a clean install every time

--- examples/run.bat
+++ examples/run.bat
@@ -1,4 +1,4 @@
 uv --version || pip install --disable-pip-version-check --break-system-packages uv
-uv pip install -r requirements.txt || uv venv && uv pip install -r requirements.txt
+uv venv && uv pip install -r requirements.txt
 uv run streamlit run basic.py
 uv run python -c "input('Press enter to exit...')"
