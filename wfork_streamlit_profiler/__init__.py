fbb2502411e277242a25ab3230275f625eae0ff7
wyattscarpenter <wyattscarpenter@gmail.com>
2025-06-07 06:00:12 -0700

    actually, pytype has its own disable syntax, so use that

--- wfork_streamlit_profiler/__init__.py
+++ wfork_streamlit_profiler/__init__.py
@@ -19,7 +19,7 @@ class Profiler(OriginalProfiler):
         self._auto_output_on_stop = auto_output_on_stop
 
     @override
-    def stop(self) -> Session: #type: ignore[override-error, unused-ignore] #workaround for: https://github.com/google/pytype/issues/1917
+    def stop(self) -> Session: #pytype: disable=override-error #workaround for: https://github.com/google/pytype/issues/1917
         session = super().stop()
 
         if self._auto_output_on_stop:
